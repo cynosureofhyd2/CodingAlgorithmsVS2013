0701985816a6f4e0c3b951acd0a256bff03d68c3
cynosureofhyd2 <tomu646@gmail.com>
2013-10-27 23:28:54 -0700

    test first commit of pixel perfect in visual studio 2013

---
+++ PixelPerfect/PixelPerfect/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PixelPerfect.MvcApplication" Language="C#" %>
---
+++ PixelPerfect/PixelPerfect/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PixelPerfect.Startup))]
+namespace PixelPerfect
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
